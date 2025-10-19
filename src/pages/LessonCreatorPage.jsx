e0c0089c2810c0201c8c051671e166d249712afb
Increase Okechukwu Divine-Wisdom <increasedwisdom@gmail.com>
2025-10-19 19:18:24 +0100

    fix: update gender options in image analysis prompt
    Specify exact gender options (Male, Female, Other) for consistent responses

--- src/pages/LessonCreatorPage.jsx
+++ src/pages/LessonCreatorPage.jsx
@@ -238,7 +238,7 @@ const LessonCreatorPage = () => {
       role: 'user',
       content: [
         { type: 'image', value: file },
-        { type: 'text', value: 'Analyze the person in this image and provide their gender, ethnicity, and a description of their facial features. For the ethnicity, you must choose from one of the following options: "American Indian or Alaska Native", "Asian", "Black or African American", "White", "Hispanic or Latino", "Middle Eastern or North African (MENA)", "Native Hawaiian or Pacific Islander". Respond in JSON format with the keys: "gender", "ethnicity", "facialFeatures".' }
+        { type: 'text', value: 'Analyze the person in this image and provide their gender ( Select from exactly: Male, Female, Other), ethnicity, and a description of their facial features. For the ethnicity, you must choose from one of the following options: "American Indian or Alaska Native", "Asian", "Black or African American", "White", "Hispanic or Latino", "Middle Eastern or North African (MENA)", "Native Hawaiian or Pacific Islander". Respond in JSON format with the keys: "gender", "ethnicity", "facialFeatures".' }
       ]
     }];
 
